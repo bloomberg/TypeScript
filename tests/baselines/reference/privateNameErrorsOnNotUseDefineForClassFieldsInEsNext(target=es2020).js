b57a8e0f61f083108099ab96cfa2bccf888720d3
Titian Cernicova-Dragomir <tcernicovad1@bloomberg.net>
2021-03-11 11:03:30 +0200

    Added missing static check for the containing property. Fixed other code review issues.

--- tests/baselines/reference/privateNameErrorsOnNotUseDefineForClassFieldsInEsNext(target=es2020).js
+++ tests/baselines/reference/privateNameErrorsOnNotUseDefineForClassFieldsInEsNext(target=es2020).js
@@ -1,15 +1,15 @@
 //// [privateNameErrorsOnNotUseDefineForClassFieldsInEsNext.ts]
-class Test {
+class TestWithErrors {
     #prop = 0 
-    static dd = new Test().#prop; // Err
+    static dd = new TestWithErrors().#prop; // Err
     static ["X_ z_ zz"] = class Inner {
         #foo  = 10   
         m() {
-            new Test().#prop // Err
+            new TestWithErrors().#prop // Err
         }
         static C = class InnerInner {
             m() {
-                new Test().#prop // Err
+                new TestWithErrors().#prop // Err
                 new Inner().#foo; // Err
             }
         }
@@ -17,13 +17,39 @@ class Test {
         static M(){
             return class {
                 m() {
-                    new Test().#prop // Err
+                    new TestWithErrors().#prop // Err
                     new Inner().#foo; // OK
                 }
             }
         } 
     }
 }
+
+class TestNoErrors {
+    #prop = 0 
+    dd = new TestNoErrors().#prop; // OK
+    ["X_ z_ zz"] = class Inner {
+        #foo  = 10   
+        m() {
+            new TestNoErrors().#prop // Ok
+        }
+        C = class InnerInner {
+            m() {
+                new TestNoErrors().#prop // Ok
+                new Inner().#foo; // Ok
+            }
+        }
+  
+        static M(){
+            return class {
+                m() {
+                    new TestNoErrors().#prop // OK
+                    new Inner().#foo; // OK
+                }
+            }
+        } 
+    }
+  }
 
 //// [privateNameErrorsOnNotUseDefineForClassFieldsInEsNext.js]
 "use strict";
@@ -33,25 +59,25 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     }
     return privateMap.get(receiver);
 };
-var _prop, _foo, _a;
-class Test {
+var _prop, _foo, _a, _prop_1;
+class TestWithErrors {
     constructor() {
         _prop.set(this, 0);
     }
 }
 _prop = new WeakMap();
-Test.dd = __classPrivateFieldGet(new Test(), _prop); // Err
-Test["X_ z_ zz"] = (_a = class Inner {
+TestWithErrors.dd = __classPrivateFieldGet(new TestWithErrors(), _prop); // Err
+TestWithErrors["X_ z_ zz"] = (_a = class Inner {
         constructor() {
             _foo.set(this, 10);
         }
         m() {
-            __classPrivateFieldGet(new Test(), _prop); // Err
+            __classPrivateFieldGet(new TestWithErrors(), _prop); // Err
         }
         static M() {
             return class {
                 m() {
-                    __classPrivateFieldGet(new Test(), _prop); // Err
+                    __classPrivateFieldGet(new TestWithErrors(), _prop); // Err
                     __classPrivateFieldGet(new Inner(), _foo); // OK
                 }
             };
@@ -60,8 +86,40 @@ Test["X_ z_ zz"] = (_a = class Inner {
     _foo = new WeakMap(),
     _a.C = class InnerInner {
         m() {
-            __classPrivateFieldGet(new Test(), _prop); // Err
+            __classPrivateFieldGet(new TestWithErrors(), _prop); // Err
             __classPrivateFieldGet(new _a(), _foo); // Err
         }
     },
     _a);
+class TestNoErrors {
+    constructor() {
+        var _foo_1, _a;
+        _prop_1.set(this, 0);
+        this.dd = __classPrivateFieldGet(new TestNoErrors(), _prop_1); // OK
+        this["X_ z_ zz"] = (_a = class Inner {
+                constructor() {
+                    _foo_1.set(this, 10);
+                    this.C = class InnerInner {
+                        m() {
+                            __classPrivateFieldGet(new TestNoErrors(), _prop_1); // Ok
+                            __classPrivateFieldGet(new Inner(), _foo_1); // Ok
+                        }
+                    };
+                }
+                m() {
+                    __classPrivateFieldGet(new TestNoErrors(), _prop_1); // Ok
+                }
+                static M() {
+                    return class {
+                        m() {
+                            __classPrivateFieldGet(new TestNoErrors(), _prop_1); // OK
+                            __classPrivateFieldGet(new Inner(), _foo_1); // OK
+                        }
+                    };
+                }
+            },
+            _foo_1 = new WeakMap(),
+            _a);
+    }
+}
+_prop_1 = new WeakMap();
